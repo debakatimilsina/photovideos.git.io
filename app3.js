1ff8e213021e31096a8fd9e749f08b61ebf67ce4
debaka <110485605+debakatimilsina@users.noreply.github.com>
2025-11-12 14:40:32 +0000

    ok

--- app3.js
+++ app3.js
@@ -96,7 +96,7 @@ DataApp.prototype.getHTML = function() {
     return `
         <div class="container">
             <div class="header">
-                <h2>🗃️Sample of Management System(SMS)</h2>
+                <h2>🗃️Photo & Videos/h2>
                 <p>Developed by: शिवप्रसाद आचार्य (२०८२, श्रीकृष्णजन्माष्टमी) </p>
             </div>
 
