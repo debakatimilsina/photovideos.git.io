0486442286fb2e1101282deece99a091944323cc
debaka <110485605+debakatimilsina@users.noreply.github.com>
2025-11-15 12:05:10 +0000

    ok

--- data5Url1.js
+++ data5Url1.js
@@ -1,25 +1,25 @@
 var data5Url1 = `SN,subject,description,shorturl,longurl
-0,git-Index.html,Codespace,codespace,https://github.com/debakatimilsina/photovideos.git.io
-1,git-Index.html,HTML,index.html,https://github.com/debakatimilsina/photovideos.git.io/edit/main/index.html
-2,git-Images,Images,data2Images.js,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data2Images.js
-3,git-Table1,Students,data1Trainees.js,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data1Trainees.js
-4,git-Table2,Assignments,data4Status.js,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data4Status.js
-4a,git-Table,LINK-Not4U,data5Url1.js,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data5Url1.js
-4b,git-Table ,Double LINK-Table,data5Url2.js,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data5Url2.js
-5,git-Table3,Notes,data3Links.js,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data3Links.js
-6,git-Link,Upload,to Upload files,https://github.com/debakatimilsina/photovideos.git.io/upload/main
-7,git-LockedImages,Locked images,data6IMAGESS.js,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data6IMAGESS.js
-8,git-NoteY1,Main Content 1-md,Markdown1,https://github.com/debakatimilsina/photovideos.git.io/edit/main/dataYmarkdown1.js
-9,git-NoteY2,Main Content 2-md,Markdown2,https://github.com/debakatimilsina/photovideos.git.io/edit/main/dataYmarkdown2.js
-9a,git-NoteY3,Main Content 3-md,Markdown3,https://github.com/debakatimilsina/photovideos.git.io/edit/main/dataYmarkdown3.js
-10,git-NoteX1,Main Content 1-html,HTML1,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data8html1.js
-11,git-NoteX2,Main Content 2-html,HTML2,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data8html2.js
-12,git-NoteZ1,Main Content 1-html,TXT1,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data0txt1.js
-13,git-NoteZ2,Main Content 2-html,TXT2,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data0txt2.js
-14,git-Link,Video1,videoslinks,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data9Videos1.js
-15,git-Link,Video2,wallvideos2,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data9Videos2.js
-16,git-Upload,Upload Content,upload,https://github.com/debakatimilsina/photovideos.git.io/upload/main
-17,git-Images-b,Images Duplicate,data2bImages.js,https://github.com/debakatimilsina/photovideos.git.io/edit/main/data2bImages.js
+0,git-Index.html,Codespace,codespace,https://github.com/debakatimilsina/legaldocuments.git.io
+1,git-Index.html,HTML,index.html,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/index.html
+2,git-Images,Images,data2Images.js,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data2Images.js
+3,git-Table1,Students,data1Trainees.js,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data1Trainees.js
+4,git-Table2,Assignments,data4Status.js,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data4Status.js
+4a,git-Table,LINK-Not4U,data5Url1.js,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data5Url1.js
+4b,git-Table ,Double LINK-Table,data5Url2.js,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data5Url2.js
+5,git-Table3,Notes,data3Links.js,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data3Links.js
+6,git-Link,Upload,to Upload files,https://github.com/debakatimilsina/legaldocuments.git.io/upload/main
+7,git-LockedImages,Locked images,data6IMAGESS.js,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data6IMAGESS.js
+8,git-NoteY1,Main Content 1-md,Markdown1,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/dataYmarkdown1.js
+9,git-NoteY2,Main Content 2-md,Markdown2,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/dataYmarkdown2.js
+9a,git-NoteY3,Main Content 3-md,Markdown3,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/dataYmarkdown3.js
+10,git-NoteX1,Main Content 1-html,HTML1,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data8html1.js
+11,git-NoteX2,Main Content 2-html,HTML2,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data8html2.js
+12,git-NoteZ1,Main Content 1-html,TXT1,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data0txt1.js
+13,git-NoteZ2,Main Content 2-html,TXT2,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data0txt2.js
+14,git-Link,Video1,videoslinks,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data9Videos1.js
+15,git-Link,Video2,wallvideos2,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data9Videos2.js
+16,git-Upload,Upload Content,upload,https://github.com/debakatimilsina/legaldocuments.git.io/upload/main
+17,git-Images-b,Images Duplicate,data2bImages.js,https://github.com/debakatimilsina/legaldocuments.git.io/edit/main/data2bImages.js
 
 
 `;
