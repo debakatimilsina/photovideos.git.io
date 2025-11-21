9bdf187e3646790e9ca7b31e9692873cbd4be82b
debaka <110485605+debakatimilsina@users.noreply.github.com>
2025-11-21 06:04:49 +0000

    Add all Images/*.NEF rows to data5Url2.js

--- data5Url2.js
+++ data5Url2.js
@@ -1,11 +1,28 @@
 var data5Url2 = `sno,subject,description,shorturl,longurl
-01, DSLR Photo-Kirtipur, Shiva & Debaka, open image, Images/DSC_0240.NEF
-
+01,DSC_0240,NEF raw image,open image,Images/DSC_0240.NEF
+02,DSC_0241,NEF raw image,open image,Images/DSC_0241.NEF
+03,DSC_0252,NEF raw image,open image,Images/DSC_0252.NEF
+04,DSC_0260,NEF raw image,open image,Images/DSC_0260.NEF
+05,DSC_0271,NEF raw image,open image,Images/DSC_0271.NEF
+06,DSC_0274,NEF raw image,open image,Images/DSC_0274.NEF
+07,DSC_0277,NEF raw image,open image,Images/DSC_0277.NEF
+08,DSC_0283,NEF raw image,open image,Images/DSC_0283.NEF
+09,DSC_0298,NEF raw image,open image,Images/DSC_0298.NEF
+10,DSC_0302,NEF raw image,open image,Images/DSC_0302.NEF
+11,DSC_0305,NEF raw image,open image,Images/DSC_0305.NEF
+12,DSC_0310,NEF raw image,open image,Images/DSC_0310.NEF
+13,DSC_0314,NEF raw image,open image,Images/DSC_0314.NEF
+14,DSC_0320,NEF raw image,open image,Images/DSC_0320.NEF
+15,DSC_0321,NEF raw image,open image,Images/DSC_0321.NEF
+16,DSC_0327,NEF raw image,open image,Images/DSC_0327.NEF
+17,DSC_0346,NEF raw image,open image,Images/DSC_0346.NEF
+18,DSC_0349,NEF raw image,open image,Images/DSC_0349.NEF
+19,DSC_0386,NEF raw image,open image,Images/DSC_0386.NEF
 `;
 
 // Dataset metadata
 var data5Url2Info = {
-    name: "legal docs -2urls",
+    name: "DSLR-Images",
     description: "Data elements with short and long url",
     emoji: "🔗",
     columns: 5,
