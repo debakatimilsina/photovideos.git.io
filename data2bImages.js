85d2444578f3dd4c92de571f62107a1e07b25dce
debaka <110485605+debakatimilsina@users.noreply.github.com>
2025-11-21 04:42:42 +0000

    ok

--- data2bImages.js
+++ data2bImages.js
@@ -1,8 +1,35 @@
 // data2bImages.js - Images Dataset (duplicate of data2Images.js)
 // Renamed variables to avoid global name collisions when both files are loaded.
 var data2bImages = `ID,Title,Description,Image
-001, अकलादेवी,अकलादेव मन्दिर-लामाचौर, Images/akaladevi3.jpg
-002, शिवप्रसाद आचार्य,webpage developer,Images/myphoto.jpg
+001,20210710_083405,,Images/20210710_083405.jpg
+002,20210710_083752,,Images/20210710_083752.jpg
+003,20211115_092012,,Images/20211115_092012.jpg
+004,अकलादेवी,,Images/akaladevi3.jpg
+005,ba aama,,Images/ba aama.jpg
+006,ba salikaram timilsina,,Images/ba salikaram timilsina.jpg
+007,harjuba,,Images/harjuba.jpg
+008,me,,Images/me.png
+009,myphoto,,Images/myphoto.jpg
+010,sameer bhai photo,,Images/sameer bhai photo.PNG
+011,DSC_0240,,Images/DSC_0240.NEF
+012,DSC_0241,,Images/DSC_0241.NEF
+013,DSC_0252,,Images/DSC_0252.NEF
+014,DSC_0260,,Images/DSC_0260.NEF
+015,DSC_0271,,Images/DSC_0271.NEF
+016,DSC_0274,,Images/DSC_0274.NEF
+017,DSC_0277,,Images/DSC_0277.NEF
+018,DSC_0283,,Images/DSC_0283.NEF
+019,DSC_0298,,Images/DSC_0298.NEF
+020,DSC_0302,,Images/DSC_0302.NEF
+021,DSC_0305,,Images/DSC_0305.NEF
+022,DSC_0310,,Images/DSC_0310.NEF
+023,DSC_0314,,Images/DSC_0314.NEF
+024,DSC_0320,,Images/DSC_0320.NEF
+025,DSC_0321,,Images/DSC_0321.NEF
+026,DSC_0327,,Images/DSC_0327.NEF
+027,DSC_0346,,Images/DSC_0346.NEF
+028,DSC_0349,,Images/DSC_0349.NEF
+029,DSC_0386,,Images/DSC_0386.NEF
 `;
 
 // Dataset metadata
