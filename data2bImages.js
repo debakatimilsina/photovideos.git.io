364c3f2fd5b4908caa1bfbdeddb1b20c852a024d
debaka <110485605+debakatimilsina@users.noreply.github.com>
2025-11-21 05:10:37 +0000

    Move web images into Images/web and update dataset paths

--- data2bImages.js
+++ data2bImages.js
@@ -1,35 +1,35 @@
 // data2bImages.js - Images Dataset (duplicate of data2Images.js)
 // Renamed variables to avoid global name collisions when both files are loaded.
 var data2bImages = `ID,Title,Description,Image
-001,20210710_083405,,Images/20210710_083405.jpg
-002,20210710_083752,,Images/20210710_083752.jpg
-003,20211115_092012,,Images/20211115_092012.jpg
-004,अकलादेवी,,Images/akaladevi3.jpg
-005,ba aama,,Images/ba aama.jpg
-006,ba salikaram timilsina,,Images/ba salikaram timilsina.jpg
-007,harjuba,,Images/harjuba.jpg
-008,me,,Images/me.png
-009,myphoto,,Images/myphoto.jpg
-010,sameer bhai photo,,Images/sameer bhai photo.PNG
-011,DSC_0240,,Images/DSC_0240.jpg
-012,DSC_0241,,Images/DSC_0241.jpg
-013,DSC_0252,,Images/DSC_0252.jpg
-014,DSC_0260,,Images/DSC_0260.jpg
-015,DSC_0271,,Images/DSC_0271.jpg
-016,DSC_0274,,Images/DSC_0274.jpg
-017,DSC_0277,,Images/DSC_0277.jpg
-018,DSC_0283,,Images/DSC_0283.jpg
-019,DSC_0298,,Images/DSC_0298.jpg
-020,DSC_0302,,Images/DSC_0302.jpg
-021,DSC_0305,,Images/DSC_0305.jpg
-022,DSC_0310,,Images/DSC_0310.jpg
-023,DSC_0314,,Images/DSC_0314.jpg
-024,DSC_0320,,Images/DSC_0320.jpg
-025,DSC_0321,,Images/DSC_0321.jpg
-026,DSC_0327,,Images/DSC_0327.jpg
-027,DSC_0346,,Images/DSC_0346.jpg
-028,DSC_0349,,Images/DSC_0349.jpg
-029,DSC_0386,,Images/DSC_0386.jpg
+001,20210710_083405,,Images/web/20210710_083405.jpg
+002,20210710_083752,,Images/web/20210710_083752.jpg
+003,20211115_092012,,Images/web/20211115_092012.jpg
+004,अकलादेवी,,Images/web/akaladevi3.jpg
+005,ba aama,,Images/web/ba aama.jpg
+006,ba salikaram timilsina,,Images/web/ba salikaram timilsina.jpg
+007,harjuba,,Images/web/harjuba.jpg
+008,me,,Images/web/me.png
+009,myphoto,,Images/web/myphoto.jpg
+010,sameer bhai photo,,Images/web/sameer bhai photo.PNG
+011,DSC_0240,,Images/web/DSC_0240.jpg
+012,DSC_0241,,Images/web/DSC_0241.jpg
+013,DSC_0252,,Images/web/DSC_0252.jpg
+014,DSC_0260,,Images/web/DSC_0260.jpg
+015,DSC_0271,,Images/web/DSC_0271.jpg
+016,DSC_0274,,Images/web/DSC_0274.jpg
+017,DSC_0277,,Images/web/DSC_0277.jpg
+018,DSC_0283,,Images/web/DSC_0283.jpg
+019,DSC_0298,,Images/web/DSC_0298.jpg
+020,DSC_0302,,Images/web/DSC_0302.jpg
+021,DSC_0305,,Images/web/DSC_0305.jpg
+022,DSC_0310,,Images/web/DSC_0310.jpg
+023,DSC_0314,,Images/web/DSC_0314.jpg
+024,DSC_0320,,Images/web/DSC_0320.jpg
+025,DSC_0321,,Images/web/DSC_0321.jpg
+026,DSC_0327,,Images/web/DSC_0327.jpg
+027,DSC_0346,,Images/web/DSC_0346.jpg
+028,DSC_0349,,Images/web/DSC_0349.jpg
+029,DSC_0386,,Images/web/DSC_0386.jpg
 `;
 
 // Dataset metadata
